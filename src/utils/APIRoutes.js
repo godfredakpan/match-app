089add75f427438aed931bb2c0a813c8c37aaba3
Godfred Akpan <godfredakpan@gmail.com>
2023-05-10 20:26:41 +0100

    update controler and api url

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
-// export const host = "https://match-app-vibes.herokuapp.com";
-export const host = "http://localhost:5050";
+export const host = "https://match-app-vibes.herokuapp.com";
+// export const host = "http://localhost:5050";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const updateUserRoute = `${host}/api/auth/updateUser`;
