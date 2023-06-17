f604e9027196b44e8f5b3f7799b027b3d644eb5e
Godfred Akpan <godfredakpan@gmail.com>
2023-06-17 21:50:51 +0100

    updated route url

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://match-app-vibes.herokuapp.com";
+export const host = "https://flirtdatingmatch.eu-4.evennode.com";
 // export const host = "http://localhost:5050";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
